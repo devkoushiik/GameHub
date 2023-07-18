ce2ff6fb1ba87e0f67d50d8ebf8542df29b3ada6
Koushik Ahmed <koushikxahmed@gmail.com>
2023-07-18 19:33:36 +0600

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+function App() {
+  return (
+    <>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
+}
+
+export default App;
