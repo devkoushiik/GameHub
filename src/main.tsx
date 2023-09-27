ba5832879b8ec6ae463d203745f00503c038fdec
Koushik Ahmed <koushikxahmed@gmail.com>
2023-09-27 22:08:58 +0600

    platform and all plat form fetching done

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,20 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import theme from "../theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement as Element).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
